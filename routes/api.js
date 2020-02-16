538b44ecb47bb2bc818ba07d1c4f234e3672e352
Aaron Diable <aaron.diable@gmail.com>
2020-02-16 19:49:40 +0000

    Auth working

--- routes/api.js
+++ routes/api.js
@@ -5,18 +5,35 @@ const SECRET = "Lets934ieslfidshk4-dsgp94euihrdssle";
 const cors = require('cors');
 const { client, pool } = require('../db/db_config');
 const jwt = require("jsonwebtoken");
-router.get('/', async (req, res, next) => {
 
-    try {
-        const result = await pool.query('SELECT * FROM users');
-        return res.json(result.rows);
-    } catch (error) {
-        return next(error)
-    }
+
+router.post('/', verifyToken , async (req, res, next) => {
+    const result = await pool.query('SELECT * FROM users');
+
+    jwt.verify(req.token, SECRET, (err, authData) => {
+        if (err) {
+            res.json ({
+                statusLock: '✋🏽',
+                message: 'You are not authorised to access this resource'
+            })
+        } else {
+            try {
+                return res.json({
+                    statusLock: '🔐',
+                    data: result
+                });
+            } catch (error) {
+                return next(error)
+            }
+        }
+
+    })
+
+
 });
 
-router.post('/adduser', async(req, res,next) => {
-    const {username, email, password} = req.body;
+router.post('/adduser', async (req, res,next) => {
+    const {username, email, password } = req.body;
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -27,13 +44,14 @@ router.post('/adduser', async(req, res,next) => {
         })
     } catch (error) {
         return res.json({
-            lockStatus: "😭",
-            message: "User NOT Saved",
+            lockStatus: "✋🏽",
+            message: "You are not authorised!",
             error
         });
-
     }
 });
+
+
 
 
 router.post('/login', async (req, res, next) => {
@@ -53,7 +71,6 @@ router.post('/login', async (req, res, next) => {
             password, foundUser.rows[0].password
         );
 
-        console.log(hashedPassword);
         if (hashedPassword === true) {
 
             const JWT = jwt.sign({
@@ -74,9 +91,6 @@ router.post('/login', async (req, res, next) => {
                 message: 'Passwords did NOT Match!'
             });
         }
-
-
-
     } catch (error) {
         return res.json({
             lockStatus: "😭",
@@ -84,13 +98,20 @@ router.post('/login', async (req, res, next) => {
             error
         });
     }
-
-
 });
 
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
 
-
-
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 
 
